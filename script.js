43ea84990dc71930cff85ac7d40aa546a3322430
Jmlor2h <112099097+Jmlor2h@users.noreply.github.com>
2022-10-03 10:41:35 -0400

    2nd update

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 //* adds & removes responsive class to/from top nav when icon is clicked. */
 function myFunction() {
-    var x = document.getElementById("topnavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("topnavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
   }
+}
 
-
-  let images = [
-    {
-        imageUrl:`images/white.png`
-    },
-    {
-        imageUrl:`images/blue.png`
-    },
-    {
-        imageUrl:`images/black.png`
-    }
-  ]
-
-  function slideShow(imageIndex){
-    document.getElementById('img1').src=images[imageIndex].imageUrl;
+let images = [
+  {
+      imageUrl:`images/ecommerceimg3.png`
+  },
+  {
+      imageUrl:`images/ecommerceimg1.png`
+  },
+  {
+      imageUrl:`images/ecommerceimg2.webp`
   }
-  function startUp(){
-    document.getElementById('img1').src=images[0].imageUrl;
-  }
+]
+let showImage = 0;
+function slideShow(imageIndex){
+  document.getElementById('img1').src=images[imageIndex].imageUrl;
+}
+function startUp(){
+  document.getElementById('img1').src=images[showImage].imageUrl;
+}
+
+setInterval(() => {
+startUp();
+showImage++;
+if (showImage == images.length){
+  showImage = 0
+};
+}, 5000)
